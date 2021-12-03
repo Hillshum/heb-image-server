d25ef3247f4ad00970be1f42654c0e14237a1634
Hilton Shumway <hillshum@gmail.com>
2021-12-03 17:49:18 -0600

    Add skeleton endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@ import express from 'express'
 const app = express();
 
 
-app.listen(process.env.PORT || 4040);
+app.get('/images', (req, res)=> {res.sendStatus(200)})
+
+const port =process.env.PORT || 4040 
+app.listen(port, ()=> {
+    console.log(`server started listening on ${port}`)
+});
